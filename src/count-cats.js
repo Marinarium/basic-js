9bf6f25b00dce5593ab8db94d33f5ae6a2e57dea
Marinarium <schwmk@gmail.com>
2020-10-04 01:15:14 +0300

    refactor: change regexp

--- src/count-cats.js
+++ src/count-cats.js
@@ -2,7 +2,7 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(matrix) {
   let arr = [];
-  let regexp = /^\^\^$/g;
+  let regexp = /^\^\^/;
   if (matrix) {
     matrix.forEach(function(item, i){
       arr = arr.concat(item.filter(elem => regexp.test(elem) ));
