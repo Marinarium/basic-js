85f2e0d4e47b671d698e22b9096244f5bc772c3d
Marinarium <schwmk@gmail.com>
2020-10-04 14:26:11 +0300

    init: dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    if (!(members instanceof Array)) return false;
+    const membersNames = members.filter(memeber => {
+        if (typeof memeber === 'string') {
+            return memeber;
+        }
+    });
+
+    const firstLetters = membersNames.map(name => name.trim().charAt(0).toUpperCase());
+
+    return firstLetters.sort().join('');
 };
