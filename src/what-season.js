a15e2878cc9f06bca4f87ce01b9a3d1aff3d0f5d
Marinarium <schwmk@gmail.com>
2020-10-11 23:26:28 +0300

    init: what season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if(!date) return 'Unable to determine the time of year!';
+  if(!getMonth(date)) return 'THROWN';
+  if (getMonth(date) < 3){
+    return 'winter';
+  } else if (getMonth(date) < 6) {
+    return 'spring';
+  } else if (getMonth(date) < 9) {
+    return 'summer';
+  } else if (getMonth(date) < 12) {
+    return 'fall';
+  } else {
+    return 'THROWN';
+  }
 };
+
+
+//module.exports = function getSeason(/* date */) {
+  //throw new CustomError('Not implemented');
+  // remove line with error and write your code here
+//};
