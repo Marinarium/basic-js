f543e00656b4288c100c13679b53e1fd6c0866fa
Marinarium <schwmk@gmail.com>
2020-10-11 23:48:52 +0300

    fix: bug with winter

--- src/what-season.js
+++ src/what-season.js
@@ -2,7 +2,7 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
   if(!date) return 'Unable to determine the time of year!';
-  if (date.getMonth() < 2){
+  if (date.getMonth() < 2 || date.getMonth() == 11){
     return 'winter';
   } else if (date.getMonth() < 5) {
     return 'spring';
