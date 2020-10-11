7f8dcce7b0eaf2a2e5da98b56de2aca00df14f59
Marinarium <schwmk@gmail.com>
2020-10-12 00:10:56 +0300

    fix: check on tricky duck

--- src/what-season.js
+++ src/what-season.js
@@ -2,6 +2,7 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
   if(!date) return 'Unable to determine the time of year!';
+  if(!(date.getTime())) return 'THROWN';
   if (date.getMonth() < 2 || date.getMonth() == 11){
     return 'winter';
   } else if (date.getMonth() < 5) {
@@ -10,9 +11,7 @@ module.exports = function getSeason(date) {
     return 'summer';
   } else if (date.getMonth() < 11) {
     return 'fall';
-  } else {
-    return 'THROWN';
-  }
+  } 
 };
 
 
