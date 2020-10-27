9021a79233d780b3767e3bf49bb83af873cd3ba2
Marinarium <schwmk@gmail.com>
2020-10-27 22:24:41 +0300

    init: transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+if (!Array.isArray(arr)) {
+  throw new Error('Not Array!');
+} else {
+
+
+let newArr = [];
+
+arr.forEach(function (item, i, array) {
+
+  if (item !== "--double-next" && item !== "--discard-next" && item !== "--double-prev" && item !== "--discard-prev") {
+      newArr.push(item);
+  }
+
+  if (item === "--double-next" && typeof array[i + 1] !== 'undefined') {
+      newArr.push(array[i + 1]);
+  }
+
+  if (item === "--double-prev" && array[i - 2] !== "--discard-next" && typeof array[i - 1] !== 'undefined') {
+      newArr.push(array[i - 1]);
+  }
+
+  if (array[i - 1] === "--discard-next" && typeof array[i + 1] !== 'undefined') {
+      newArr.pop();
+  }
+  if (array[i + 1] === "--discard-prev" && typeof array[i - 1] !== 'undefined' && array[i - 1] !== "--discard-next") {
+      newArr.pop();
+  }
+
+});
+
+return newArr;
+  
+}
 };
