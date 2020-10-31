773a1fceb7c74a2267dbcaba9d55111357a37cf2
Marinarium <schwmk@gmail.com>
2020-11-01 02:26:11 +0300

    feat: simple decrypt

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -64,7 +64,57 @@ class VigenereCipheringMachine {
   }    
   
   decrypt(message, key) {
-    
+    function symbolOrder(a){
+      return a.toUpperCase().charCodeAt(0) - 65;
+    }
+
+
+    let messageArr = message.split('');
+    let numbersOfMessage = [];
+    messageArr.forEach(function(item) {
+        if(symbolOrder(item) < 26 && symbolOrder(item) >= 0){
+            numbersOfMessage.push(symbolOrder(item));
+        } else {
+            numbersOfMessage.push(item);
+        }
+    });
+
+    let keyArr = key.repeat(Math.ceil(message.length / key.length)).slice(0, message.length).split('');
+    let numbersOfKey =[]
+    let j = 0;
+    numbersOfMessage.forEach(function (item, i) {
+        if (isNaN(item) || item === " ") {
+            numbersOfKey.push(item);
+        } else {
+            numbersOfKey.push(symbolOrder(keyArr[j]));
+            j++;
+        }
+    });
+
+    let digitalCode = [];
+    numbersOfMessage.forEach(function (item, i) {
+        if (isNaN(item) || item === " ") {
+            digitalCode.push(item);
+        } else if((item - numbersOfKey[i]) < 0) {
+            digitalCode.push(item + 26 - numbersOfKey[i]);
+        } else {
+            digitalCode.push(item - numbersOfKey[i]);
+        }
+    });
+
+    let cipherArr = [];
+    digitalCode.forEach(function (item) {
+        if (isNaN(item) || item === " ") {
+            cipherArr.push(item);
+        } else {
+            cipherArr.push(String.fromCharCode(item + 65));
+        }
+    });
+
+    let cipher = cipherArr.join('');
+
+
+    return cipher;
   }
 }
 
